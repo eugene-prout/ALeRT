df21c18dbcc5c05dd88912cf33b693431084d089
MacWas <maciek.waszczuk@hotmail.co.uk>
2023-08-13 13:26:53 +0100

    refac:filename generated in python
    Python now generates the filename, passing it to both Jinja and the generator script properly.
    Placeholder graph names replaced.

--- alert/blueprints/application/routes.py
+++ alert/blueprints/application/routes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template
 
 import alert.lib.script as graph_script
 
+import random
+
 from alert.forms import EditGrammarForm
 
 application = Blueprint("user", __name__)
@@ -11,7 +13,9 @@ application = Blueprint("user", __name__)
 def home():
     form = EditGrammarForm()
     if form.validate_on_submit():
-        graph_script.graph_of_grammar(form.grammar_editor.data,filename='e')
+        filename = random.randint(1,10000000)
+        graph_script.graph_of_grammar(form.grammar_editor.data,filename=str(filename))
+        return render_template("application/home.html",form=form,filename=str(filename))
 
     return render_template("application/home.html", form=form)
 
