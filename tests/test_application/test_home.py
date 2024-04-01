21bd339708af51a447ed6ed81ff935712d08a338
Eugene Prout <eugene.prout1@gmail.com>
2024-04-01 22:02:01 +0100

    Fix failing test

--- tests/test_application/test_home.py
+++ tests/test_application/test_home.py
@@ -2,7 +2,9 @@ def test_home(test_client):
     response = test_client.get("/")
 
     assert response.status_code == 200
-    assert b"This is the home page" in response.data
+    assert b"ALeRT | Grammar property visualiser" in response.data
+    assert b"1. About ALeRT" in response.data
+    assert b"Enter Your Grammar" in response.data
 
 
 def test_status(test_client):
